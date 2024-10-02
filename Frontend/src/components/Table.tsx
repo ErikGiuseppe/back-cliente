efe877c2a2170eb7005a35759bc3246ebd04077d
Erik Giuseppe Kato Bandeira <erik.setide@osasco.sp.gov.br>
2024-10-02 09:37:26 -0300

    arrumando button

--- Frontend/src/components/Table.tsx
+++ Frontend/src/components/Table.tsx
@@ -106,13 +106,7 @@ function EnhancedTableHead(props: EnhancedTableProps) {
         <TableHead>
             <TableRow>
                 <TableCell padding="checkbox">
-                    <Checkbox
-                        color="primary"
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{ 'aria-label': 'select all desserts' }}
-                    />
+                    
                 </TableCell>
                 {headCells.map((headCell) => (
                     <TableCell
